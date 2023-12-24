9a2cf4f43fa6d2364966c8b1fb348fe6dd36c486
inam6565 <inamrehman6565@gmail.com>
2023-12-24 15:34:13 +0500

    added upadate and delete calls

--- mern-server/index.js
+++ mern-server/index.js
@@ -15,14 +15,14 @@ app.get('/', (req, res) => {
 
 //mongodb configuration
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = "mongodb+srv://mern-book-store:ii53MXFhYaqjQV5a@cluster0.xt3rpcn.mongodb.net/?retryWrites=true&w=majority";
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
-    strict: true,
+    strict: true, 
     deprecationErrors: true,
   }
 });
@@ -31,6 +31,49 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    //create  a collection of documents
+    const bookCollection = client.db("BookInventory").collection("books");
+
+    //insert a book to the db: post method
+    app.post("/upload-book", async(req, res) => {
+        const data = req.body;
+        const result = await bookCollection.insertOne(data);
+        res.send(result);
+    })
+
+    //Get all books from database
+    app.get("/all-books", async(req, res) => {
+        const books = bookCollection.find();
+        const result = await books.toArray();
+        res.send(result);
+    })
+
+    //update a book data : patch and update methods
+    app.patch("/book/:id", async(req,res) => {
+        const id = req.params.id;
+        //console.log(id);
+        const updateBookData = req.body;
+        const filter = {_id: new ObjectId(id)};
+        const updateDoc = {
+            $set: {
+                ...updateBookData
+            }
+        }
+        const options = { upcert: true};
+        //update
+        const result = await bookCollection.updateOne(filter, updateDoc, options);
+        res.send(result)
+    })
+
+    //Delete a book
+    app.delete("/book/:id", async(req,res) => {
+      const id = req.params.id;
+      const filter = {_id: new ObjectId(id)};
+      const result = await bookCollection.deleteOne(filter);
+      res.send(result);
+  })
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");

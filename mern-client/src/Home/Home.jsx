6b0eb2f03959d6d829335140b6779eeba5c1ac88
inam6565 <inamrehman6565@gmail.com>
2023-12-27 00:00:56 +0500

    added BookCards

--- mern-client/src/Home/Home.jsx
+++ mern-client/src/Home/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Banner from '../components/Banner'
+import FavouriteBooks from './FavouriteBooks'
 
 const Home = () => {
   return (
     <div className=''>
       <Banner/>
+      <FavouriteBooks/>
     </div>
   )
 }
